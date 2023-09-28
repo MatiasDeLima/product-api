ebeee49bb47f07686ede1e8aa19608d23542e644
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2023-09-28 08:45:48 -0300

    feat(initialize): initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import productRouter from "./routes/productsRoutes.js"
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_SECRET_KEY, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log("MongoDB Connect TRUE!");
+    } catch (error) {
+        console.log("MongoDB Connect FALSE!", error);
+    }
+}
+
+app.use(express.json());
+app.use("/api/v1/products", productRouter);
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Server listening on port: ${port}`);
+})
