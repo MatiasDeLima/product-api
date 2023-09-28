9ebe2895587d3d4bddd3ea184e08b2295fb14a11
Vitu <loic.lemos@gmail.com>
2023-09-28 20:37:23 -0300

    controllers

---
+++ controller/productController.js
@@ -0,0 +1,102 @@
+import Product from "../models/Product.js";
+
+
+// create product
+export const createProduct = async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully to created",
+            data: savedProduct
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to created"
+        })
+    }
+}
+
+// updated product
+export const updatedProduct = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, 
+        { $set: req.body },    
+        { new: true }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully to upadated",
+            body: updatedProduct
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to updated"
+        })
+    }
+}
+
+// delete product
+export const deleteProduct = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await Product.findByIdAndDelete(id);
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully to deleted"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to deleted"
+        })
+    }
+}
+
+// get single product
+export const getSingleProduct = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const product = await Product.findById(id);
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully to find on",
+            data: product
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to find one"
+        })
+    }
+}
+
+// get all products
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully to find all",
+            data: products
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to find all"
+        })
+    }
+}
